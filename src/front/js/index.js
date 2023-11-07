0f688632d4379cf0d07a70c0740d987c5e3857be
Carlos Daniel Ponce <135666468+carlosp-11@users.noreply.github.com>
2023-11-07 20:55:50 +0000

    feat: bootswatch slate theme

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.css";  // Include your index.scss file into the bundle
 import Layout from "./Layout.jsx";  // Import your own components
-
+import 'bootswatch/dist/slate/bootstrap.min.css'; // Importamos paleta de colores slate
 
 // Render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
