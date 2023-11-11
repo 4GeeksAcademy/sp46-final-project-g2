52d366bbd058c6dfe5a42321afa19d58b75b925c
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-11 10:16:38 +0000

    feat: login.jsx

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
+      user: {},
       message: null,
       demo: [{title: "FIRST", background: "white",initial: "white"},
              {title: "SECOND",background: "white",initial: "white"}]
