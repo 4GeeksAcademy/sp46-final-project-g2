e2ed2faa7327fc3c9230931a1bf378e4187679b4
Carlos Daniel Ponce <135666468+carlosp-11@users.noreply.github.com>
2023-11-11 15:19:39 +0000

    feat: cards, AuthorList, AuthorProfile

--- src/front/js/component/Navbar-carlos.jsx
+++ src/front/js/component/Navbar-carlos.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 
 export const Navbar = () => {
 	return (
 		<nav class="navbar navbar-expand-md navbar-dark">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
 					<img src={Logo} alt="LiteraryInk logo" style={{ width: '8rem' }} />
 				</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLiteraryInk" aria-controls="navbarLiteraryInk" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLiteraryInk" aria-controls="navbarLiteraryInk" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
 				</button>				
-				<div class="collapse navbar-collapse " id="navbarLiteraryInk">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<div className="collapse navbar-collapse " id="navbarLiteraryInk">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
 							<Link to="/categories">
-								<button className="btn border border-0 mt-1"> Categorías </button>
+								<button className="btn border border-0 mt-1 "> Categorías </button>
 							</Link>
 						</li>
 						<li className="nav-item">
-							<Link to="/authors">
+							<Link to="/list">
 								<button className="btn border border-0 mt-1"> Autores </button>
 							</Link>
 						</li>
-						<li class="nav-item dropdown ">
-							<a class="nav-link dropdown-toggle border border-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						<li className="nav-item dropdown ">
+							<a className="nav-link dropdown-toggle border border-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 								Servicios
 							</a>
 							<ul class="dropdown-menu">
@@ -35,14 +37,12 @@ export const Navbar = () => {
 								<li><a className="dropdown-item" href="Talleres"> Talleres </a></li>
 							</ul>
 						</li>
-						<li>
-							<form role="search">
-								<input className="form-control" type="search" placeholder="Buscar" />
-							</form>
-						</li>
 					</ul>
+					<Link to="/">
+						<button className="btn border-0 mx-3"> <FontAwesomeIcon icon={faMagnifyingGlass} size="xl"/>  </button>
+					</Link>
 					<Link to="/login">
-						<button className="btn btn-warning"> Ingresa </button>
+						<button className="btn btn-warning fw-bold text-dark"> Ingresa </button>
 					</Link>
 				</div>
 			</div>
---
+++ src/front/js/component/PostCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashnode } from "@fortawesome/free-brands-svg-icons";
+import { faGlasses } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+
+export const PostCard = () => {
+    return (
+
+        <div className="card my-1 mx-2" >
+        <div className="card-body">
+          <h5 className="card-title">El mito de Sísifo</h5>
+          <h6 className="card-subtitle mb-4 text-body-secondary fw-lighter"> <FontAwesomeIcon icon={faHashnode} size= "sm"/>  Ensayo - Prosa - Existencialismo </h6>
+          <p className="card-text text-truncate">El título del ensayo proviene de un atribulado personaje de la mitología griega. 
+          En él, Camus discute la cuestión del suicidio y el valor de la vida, presentando el mito de Sísifo como 
+          metáfora del esfuerzo inútil e incesante del hombre. De esta forma plantea la filosofía del absurdo, que 
+          mantiene que nuestras vidas son insignificantes y no tienen más valor que el de lo que creamos. Siendo el 
+          mundo tan fútil, Camus pregunta, ¿hay alternativa al suicidio? El ensayo se inicia: No hay sino un problema 
+          filosófico realmente serio: el suicidio.</p>
+          <h6 className="card-subtitle fw-lighter mb-2"> <FontAwesomeIcon icon={faGlasses}  size= "sm" /> <span className="text-light"> 751 </span> veces leído </h6>
+          <h6 className="card-subtitle fw-lighter mb-2"> <FontAwesomeIcon icon={faStar}  size= "sm" /><FontAwesomeIcon icon={faStar}  size= "sm" /><FontAwesomeIcon icon={faStar}  size= "sm" />
+          <FontAwesomeIcon icon={faStar}  size= "sm" /><FontAwesomeIcon icon={faStarHalfStroke}  size= "sm" /> - 4,5
+          </h6>
+        </div>
+      </div>
+    );
+};
