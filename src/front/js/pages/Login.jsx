ecd987516c5577f5b7807bb0a98e5c931a68eab1
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-12 14:20:45 +0000

    feat: login.jsx added alert component

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -10,35 +10,32 @@ export const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [viewPassword, setViewPassword] = useState(false)
-    const [ alert, setAlert ] = useState({show: false, message: ''})
+    const [alert, setAlert] = useState({ show: false, message: '' })
     const navigate = useNavigate();
 
-
-    // No utilizar useContext. Utilizar useState
-    // Realizar el fetch (al enpoint login) dentro de la función de handleOnSubmit (igual será para el SignUp signup advisor)
     const handleEmail = (event) => setEmail(event.target.value);
     const handlePassword = (event) => setPassword(event.target.value);
 
     const login = async () => {
-        setAlert({show: false, message: ''})
+        setAlert({ show: false, message: '' })
         const url = process.env.BACKEND_URL + '/api/login';
         const options = {
             method: 'POST',
             body: JSON.stringify({ email, password }),
-            headers: {'Content-Type': 'application/json'}
+            headers: { 'Content-Type': 'application/json' }
         }
         const response = await fetch(url, options);
         if (response.ok) {
             const data = await response.json();
             localStorage.setItem('token', data.token);
             store.user = data.results;
-            setAlert({show: true, message: 'User logged'});
+            setAlert({ show: true, message: 'User logged' });
             console.log(data)
             navigate("/");
         } else {
             if (response.status == 404) {
                 const error = await response.text();
-                setAlert({show: true, message: 'Bad user or password'});
+                setAlert({ show: true, message: 'Bad user or password' });
             }
             console.log('Error: ', response.status, response.statusText);
             /* tratar el error */
@@ -62,20 +59,22 @@ export const Login = () => {
                             className="form-control" id="exampleInputPassword1" placeholder="Password" />
                     </div>
                     {/* 
-                <div className="form-group">
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="dropdownCheck" />
-                        <label className="form-check-label" htmlFor="dropdownCheck">
-                            Remember me
-                        </label>
+                    <div className="form-group">
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="dropdownCheck" />
+                            <label className="form-check-label" htmlFor="dropdownCheck">
+                                Remember me
+                            </label>
+                        </div>
                     </div>
-                    </div>
-                */}
+                    */}
                     <button type="button" onClick={login} className="btn btn-primary btn-block">Log in</button>
                     <div className="dropdown-divider"></div>
                     <Link className="dropdown-item" to="/signup">Sign up</Link>
-                    {/* <Link className="dropdown-item" to="/signup">New around here? Sign up</Link>
-            <Link className="dropdown-item" href="#">Forgot password?</Link> */}
+                    {/* 
+                    <Link className="dropdown-item" to="/signup">New around here? Sign up</Link>
+                    <Link className="dropdown-item" href="#">Forgot password?</Link>
+                    */}
                 </form>
             </div>
         </div>
