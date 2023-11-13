a70f45d4abb47ecb309a7f9e5e4e3234464d7a9a
Elisa <135215841+ElisaGDR@users.noreply.github.com>
2023-11-13 22:52:40 +0000

    Services done

--- src/front/js/pages/PenToPrint.jsx
+++ src/front/js/pages/PenToPrint.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-export const Mentories = () => {
+export const PenToPrint = () => {
     return (
         <div className="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
             <div className="my-3 py-3">
-                <h2 className="display-5">Mentorías</h2>
-                <p className="lead">¿Quieres publicar tu libro y no sabes cómo?</p>
+                <h2 className="display-5">Pen to Print</h2>
+                <p className="lead">Transcriba sus notas manuscritas a texto digital.</p>
             </div>
             <div className="container mt-4">
                 <div className="row">
@@ -31,31 +31,23 @@ export const Mentories = () => {
                                     lineHeight: '1.5',
                                     maxWidth: '80%',
                                 }}>
-                                Nos comprometemos a acompañarte durante todo el proceso.
-                                Tú decides el momento idóneo para iniciar este camino.
-                                Desde el despliegue de tu fase creativa hasta la exploración de opciones editoriales y
-                                otros recursos para concretar tu obra literaria.
+                                Con Pen to Print, cualquier palabra escaneada, imagen o archivo PDF se puede convertir de escritura
+                                a mano a texto o documento. Las aplicaciones móviles y el reconocimiento óptico de caracteres
+                                (OCR) en línea de Pen to Print están diseñados específicamente para reconocer la escritura a mano
+                                y funcionan de manera excelente como lector de cursiva y para descifrar escritura ilegible al
+                                convertir notas a texto.
                             </p>
                             <div className="row">
-                                <div className="col-md-4 m-2" style={{ border: '2px solid black', borderRadius: '21px 21px 0 0' }}>
-                                    <div className="card mb-4 box-shadow" >
-                                        <h4 className="my-0 font-weight-normal">Taller completo</h4>
-                                    </div>
-                                    <div className="card-body">
-                                        <h1 className="card-title pricing-card-title" style={{ fontSize: '20px', color: 'black' }}>359€</h1>
-                                        <button type="button" className="btn btn-warning fw-bold text-dark mr-2 mt-4">Comprar</button>
-                                    </div>
-                                </div>
-                                <div className="col-md-5 m-2" style={{ border: '2px solid black', borderRadius: '21px 21px 0 0' }}>
+                                <div className="col-md-8 m-2" style={{ border: '2px solid black', borderRadius: '21px 21px 0 0' }}>
                                     <div className="card mb-4 box-shadow" >
                                         <h4 className="my-0 font-weight-normal">LiteraryInk Miembro</h4>
                                     </div>
                                     <div className="card-body">
                                         <h1 className="card-title pricing-card-title" style={{ fontSize: '20px', color: 'black' }}>23,95€/mes</h1>
                                         <ul className="list-unstyled mt-3 mb-4" style={{ color: 'black' }}>
-                                            <li>20% Dto en talleres</li>
-                                            <li>3 reviews incluidas</li>
                                             <li>Acceso a Pen to Write</li>
+                                            <li>20€ Dto en talleres</li>
+                                            <li>3 reviews incluidas</li>
                                         </ul>
                                         <button type="button" className="btn btn-warning fw-bold text-dark mr-2 mb-4">Hazte miembro</button>
                                     </div>
@@ -68,9 +60,8 @@ export const Mentories = () => {
                             <div className="card-body">
                                 <h2 className="card-title">Te ofrecemos:</h2>
                                 <ul className="list" style={{ textAlign: 'justify', fontSize: '15px' }}>
-                                    <li>Sesiones de mentoría online de 50 minutos cada una.</li>
-                                    <li>Selección personalizada del mentor de entre nuestros profesionales.</li>
-                                    <li>Flexibilidad para adaptarnos a tu horario y a tus necesidades específicas.</li>
+                                    <li>100 páginas escaneadas.</li>
+                                    <li>Descargar como archivos de texto o Word, o copiar al portapapeles.</li>
                                 </ul>
                             </div>
                         </div>
